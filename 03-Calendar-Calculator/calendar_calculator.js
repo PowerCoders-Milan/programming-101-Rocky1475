aece5ab488daa1cb57498db1c50b895086c638ba
Muhammad rRehman <muhammadislamian@gmail.com>
2021-02-18 14:47:33 +0000

    excercise 3 is also done

--- 03-Calendar-Calculator/calendar_calculator.js
+++ 03-Calendar-Calculator/calendar_calculator.js
@@ -1,6 +1,13 @@
 //insert your pseudocode below
 /*
-here
+i really dont know waht i did but there is a number plus another which makes 49 and i think this 
+is the date today which is february the 18. 
+and then print the date which gives you 49. 
 */
 
 //Insert your code below
+var Date2;
+
+
+Date2 = String(31 + 18) + '';
+window.alert(Date2);
