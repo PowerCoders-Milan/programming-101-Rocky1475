5e3cb16c8bbab7f775f49cfd6204efc613d24ab8
Muhammad rRehman <muhammadislamian@gmail.com>
2021-02-18 17:16:37 +0000

    3rd Ex half done

--- 03-Calendar-Calculator/calendar_calculator.js
+++ 03-Calendar-Calculator/calendar_calculator.js
@@ -1,13 +1,35 @@
 //insert your pseudocode below
 /*
-i really dont know waht i did but there is a number plus another which makes 49 and i think this 
-is the date today which is february the 18. 
-and then print the date which gives you 49. 
+give any date we need to find which number of the year that date 
+example: 18th feb is equal to 31 + 18 
+we have 3 variables 
+the first variable is the number of the month 
+second variable name of the month
+find that how many days are there before name of the month 
+third variable is  how many days are there before name of the month 
+we need to add number of the month to the days of the months before
+print the result. 
 */
 
 //Insert your code below
-var Date2;
+var numbers;
+
+function listsGetSortCompare(type, direction) {
+    var compareFuncs = {
+        "NUMERIC": function (a, b) {
+            return Number(a) - Number(b);
+        },
+        "TEXT": function (a, b) {
+            return a.toString() > b.toString() ? 1 : -1;
+        },
+        "IGNORE_CASE": function (a, b) {
+            return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1;
+        },
+    };
+    var compare = compareFuncs[type];
+    return function (a, b) { return compare(a, b) * direction; }
+}
 
 
-Date2 = String(31 + 18) + '';
-window.alert(Date2);
+numbers = [3, 1, 5];
+window.alert(numbers.slice().sort(listsGetSortCompare("NUMERIC", 1)));
